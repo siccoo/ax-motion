6e248477e0eb65f026d4b9407f82167fd7c408c6
Michael Chilaka <obichilaka@gmail.com>
2022-01-31 14:44:44 +0100

    updates on ImgContainer.js & style.css

--- src/pages/ImgContainer.js
+++ src/pages/ImgContainer.js
@@ -1,42 +1,72 @@
 import React from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
+import AxImg1 from "../assets/images/ax-img1.jpeg";
+import AxImg2 from "../assets/images/ax-img2.jpeg";
+import AxImg3 from "../assets/images/ax-img3.jpeg";
+import AxImg4 from "../assets/images/ax-img4.jpeg";
+// import AxImg5 from "../assets/images/ax-img5.jpeg";
+import AxImg6 from "../assets/images/ax-img6.jpeg";
+import AxImg7 from "../assets/images/ax-img7.jpeg";
 import "./style.css";
 
 const axProducts = [
     {
         _id: "1",
-        image: "../assets/images/ax-img1.jpeg"
+        image: AxImg1
     },
     {
         _id: "2",
-        image: "../assets/images/ax-img2.jpeg"
+        image: AxImg2
     },
     {
         _id: "3",
-        image: "../assets/images/ax-img3.jpeg"
+        image: AxImg3
     },
     {
         _id: "4",
-        image: "../assets/images/ax-img4.jpeg"
+        image: AxImg4
     },
-    {
-        _id: "5",
-        image: "../assets/images/ax-img5.jpeg"
-    },
+    // {
+    //     _id: "5",
+    //     image: AxImg5
+    // },
     {
         _id: "6",
-        image: "../assets/images/ax-img6.jpeg"
+        image: AxImg6
+    },
+    {
+        _id: "7",
+        image: AxImg7
     }
 ];
+
+const imageVariants = {
+    visible: {
+        opacity: 1,
+        scale: 2,
+        transition: {duration: 1}                                                                                                                                                                           
+    },
+    hidden: {
+        opacity: 0,
+        scale: 0
+    }
+}
 
 const ImgContainer = () => {
     return (
         <div className='content__wrap'>
             <ul className='img__list'>
-                {axProducts.map((item) => (
+                {axProducts.map((item, i) => (
                     <li key={item._id} item={item}>
-                        <a target="_blank" href='/'>
-                            <img src={item.image} alt='ax-images' />
+                        <a target="_blank" href='#'>
+                            <motion.img 
+                                src={item.image} 
+                                alt='ax-images' 
+                                variants={imageVariants}
+                                animate={{scale: 2}}
+                            />
                         </a>
                     </li>
                 ))}
