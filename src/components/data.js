71b00d00bd8b56e8cfb79873e974c2dcc5e8bd3e
Michael Chilaka <obichilaka@gmail.com>
2022-01-31 08:01:16 +0100

    updates on data.js

---
+++ src/components/data.js
@@ -0,0 +1,28 @@
+const data = {
+  axProducts: [
+    {
+      _id: "1",
+      image: "../../assets/images/ax-img1.jpeg"
+    },
+    {
+      _id: "2",
+      image: "../../assets/images/ax-img2.jpeg"
+    },
+    {
+      _id: "3",
+      image: "../../assets/images/ax-img3.jpeg"
+    },
+    {
+      _id: "4",
+      image: "../../assets/images/ax-img4.jpeg"
+    },
+    {
+      _id: "5",
+      image: "../../assets/images/ax-img5.jpeg"
+    },
+    {
+      _id: "6",
+      image: "../../assets/images/ax-img6.jpeg"
+    }
+  ],
+};
--- src/components/imgGallery/Data.js
+++
@@ -1,9 +0,0 @@
-export const data = [
-    { id: 1, image: "../../assets/images/ax-img1.jpeg" },
-    { id: 2, image: "../../assets/images/ax-img2.jpeg" },
-    { id: 3, image: "../../assets/images/ax-img3.jpeg" },
-    { id: 4, image: "../../assets/images/ax-img4.jpeg" },
-    { id: 5, image: "../../assets/images/ax-img5.jpeg" },
-    { id: 6, image: "../../assets/images/ax-img6.jpeg" },
-    { id: 7, image: "../../assets/images/ax-img7.jpeg" }
-]
